eec6f2c5f485143be75e6a5c8e69d575018b8e2e
Kento Uchida <kento.apps@gmail.com>
2017-05-14 23:31:06 -0700

    Implement Factory class

--- AbstractFactory/src/factory/Factory.java
+++ AbstractFactory/src/factory/Factory.java
@@ -4,4 +4,19 @@ package factory;
  * Created by kento on 2017/05/12.
  */
 public abstract class Factory {
+    public static Factory getFactory(String className) {
+        Factory factory = null;
+        try {
+            factory = (Factory) Class.forName(className).newInstance();
+        } catch (ClassNotFoundException e) {
+            System.err.println("Cannot find class " + className);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return factory;
+    }
+
+    public abstract Link createLink(String caption, String url);
+    public abstract Tray createTray(String caption);
+    public abstract Page createPage(String title, String author);
 }
